e18652d4dca0d1de2dbf828614d8fa89f306de00
Pranavi <116634403+pranavi2@users.noreply.github.com>
2024-06-01 10:05:37 +0530

    Time: 15 ms (59.87%) | Memory: 11.4 MB (94.45%) - LeetSync

---
+++ 66-plus-one/plus-one.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def plusOne(self, digits):
+        if len(digits)==0:
+            return [1]
+        if digits[-1]!=9:
+            digits[-1]+=1
+            return digits
+        else:
+            digits=self.plusOne(digits[:-1])
+            digits.append(0)
+            return digits
+        
